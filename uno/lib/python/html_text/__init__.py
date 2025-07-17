d90335647633a232bccd30cb806b65cec5ec58ec
prrvchr <prrvchr@gmail.com>
2025-07-17 14:31:23 +0200

    new version 1.3.1

---
+++ uno/lib/python/html_text/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.7.0"
+
+from .html_text import (
+    DOUBLE_NEWLINE_TAGS,
+    NEWLINE_TAGS,
+    cleaned_selector,
+    cleaner,
+    etree_to_text,
+    extract_text,
+    parse_html,
+    selector_to_text,
+)
